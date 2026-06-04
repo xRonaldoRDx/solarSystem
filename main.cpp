4fbd59f25b4f694bd193b67354dc87dd62a10bc9
Ronaldo Rossetti <ronaldord.3001@gmail.com>
2026-06-04 12:10:36 -0300

    feat: add instructions text

--- main.cpp
+++ main.cpp
@@ -242,6 +242,45 @@ void desenhaSistemaSaturno(float distanciaSaturno, float tamanhoSaturno, float v
     glPopMatrix();
 }
 
+void desenhaTexto(const char* texto, int x, int y) {
+    glRasterPos2i(x, y);
+    for (const char* c = texto; *c != '\0'; c++) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c);
+    }
+}
+
+void desenhaInstrucoes() {
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+    
+    int largura = glutGet(GLUT_WINDOW_WIDTH);
+    int altura = glutGet(GLUT_WINDOW_HEIGHT);
+    gluOrtho2D(0, largura, 0, altura);
+
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glLoadIdentity();
+
+    glDisable(GL_LIGHTING);
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_TEXTURE_2D); 
+
+    glColor3f(1.0f, 1.0f, 1.0f); // Cor do texto
+    desenhaTexto("CONTROLES:", 20, altura - 40);
+    desenhaTexto("- Botao Direito + Arrastar: Girar Camera", 20, altura - 65);
+    desenhaTexto("- Scroll: Zoom In/Out", 20, altura - 90);
+
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+
+    glPopMatrix(); 
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix(); 
+    glMatrixMode(GL_MODELVIEW); 
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
@@ -277,6 +316,8 @@ void display() {
 
     desenhaPlaneta(distanciaUrano, tamanhoUrano, velocidadeUrano, texturaUrano); // Urano
     desenhaPlaneta(distanciaNetuno, tamanhoNetuno, velocidadeNetuno, texturaNetuno); // Netuno
+
+    desenhaInstrucoes();
 
     glutSwapBuffers();
 }
